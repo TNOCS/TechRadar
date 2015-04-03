6edc6ee5b24184886d7ecef599186c8141fb708f
erik <erik.vullings@gmail.com>
2015-04-03 18:56:58 +0200

    Revert "First version"
    This reverts commit 3367408466b4b38e4e797acaba7269a60efc0392.

--- public/directives/slide/slide.tpl.js
+++
@@ -1,4 +0,0 @@
-var Slide;
-(function (Slide) {
-    Slide.html = '<div>SLIDE PLACEHOLDER<br />{{vm.table}}</div>';
-})(Slide || (Slide = {}));
--- public/directives/slide/slide.tpl.ts
+++
@@ -1,1 +0,0 @@
-module Slide { export var html = '<div>SLIDE PLACEHOLDER<br />{{vm.table}}</div>'; }
