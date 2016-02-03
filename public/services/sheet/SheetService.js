d74210251ade92ac5ac80c4678c83e1e4b03182b
Erik Vullings <erik.vullings@gmail.com>
2016-02-03 15:25:23 +0100

    SheetService loads the URL and converts to Technologies.

--- public/services/sheet/SheetService.js
+++ public/services/sheet/SheetService.js
@@ -5,6 +5,62 @@ var csComp;
         var SpreadsheetService = (function () {
             function SpreadsheetService() {
             }
+            SpreadsheetService.prototype.loadTechnologies = function (url, callback) {
+                var _this = this;
+                this.loadSheet(url, function (spreadsheet) {
+                    _this.technologies = [];
+                    var id = 1;
+                    var page = 0;
+                    var technology;
+                    spreadsheet.forEach(function (row) {
+                        if (row.Category !== '') {
+                            var deltaTimeString = row.DeltaTime;
+                            var priority = parseInt(row.Priority.toString(), 10);
+                            var color;
+                            switch (priority) {
+                                case 1:
+                                    color = '#F39092';
+                                    break;
+                                case 2:
+                                    color = '#F5DC8F';
+                                    break;
+                                case 3:
+                                    color = '#9EBACB';
+                                    break;
+                                case 4:
+                                    color = '#DFE0DC';
+                                    break;
+                                default:
+                                    color = 'white';
+                                    break;
+                            }
+                            var deltaTime = 0;
+                            if (typeof deltaTimeString === 'string') {
+                                deltaTime = +deltaTimeString.replace(',', '.');
+                            }
+                            else {
+                                deltaTime = deltaTimeString;
+                            }
+                            page = 0;
+                            technology = new TechRadar.Technology(id++, priority, row.Category, row.Thumbnail, row.TimeCategory, deltaTime, row.ShortTitle, row.Title, row.Subtitle, row.Text, color);
+                            _this.technologies.push(technology);
+                        }
+                        if (row.ContentType === '')
+                            row.ContentType = 'markdown';
+                        if (row.Content !== '') {
+                            var c = new TechRadar.Content(page++, row.ContentType, row.Content, row.Subtitle);
+                            if (c.contentType.toLowerCase() === 'youtube') {
+                                c.videoUrl = c.content.indexOf('http') > 0
+                                    ? c.content
+                                    : 'http://www.youtube.com/embed/' + c.content + '?rel=0&autoplay=1';
+                            }
+                            ;
+                            technology.content.push(c);
+                        }
+                    });
+                    callback();
+                });
+            };
             SpreadsheetService.prototype.loadSheet = function (url, callback) {
                 console.log('Initializing tabletop');
                 Tabletop.init({
--- public/services/sheet/SheetService.ts
+++ public/services/sheet/SheetService.ts
@@ -2,17 +2,17 @@ module csComp.Services {
     declare var Tabletop;
 
     export  interface ISpreadsheetRow {
-        Category:      string,
-        Priority:      number,
-        Thumbnail:     string,
-        TimeCategory:  string,
-        DeltaTime:     string | number,
-        ShortTitle:    string,
-        Title:         string,
-        Text:          string,
-        Subtitle:      string,
-        ContentType:   string,
-        Content: string
+        Category:      string;
+        Priority:      number;
+        Thumbnail:     string;
+        TimeCategory:  string;
+        DeltaTime:     string | number;
+        ShortTitle:    string;
+        Title:         string;
+        Text:          string;
+        Subtitle:      string;
+        ContentType:   string;
+        Content:       string;
     }
 
     /**
@@ -20,10 +20,73 @@ module csComp.Services {
      * See: https://github.com/jsoma/tabletop
      */
     export class SpreadsheetService {
+        public technologies: TechRadar.Technology[];
+
+        /** Load the technologies */
+        public loadTechnologies(url: string, callback: () => void) {
+            this.loadSheet(url, (spreadsheet: ISpreadsheetRow[]) => {
+                this.technologies = [];
+                var id = 1;
+                var page = 0;
+                var technology;
+                spreadsheet.forEach((row) => {
+                    // check if it's part of previous
+                    if (row.Category !== '') {
+
+                        //console.log(row.Category);
+                        //console.log(row.Title);
+                        var deltaTimeString = row.DeltaTime;
+                        var priority = parseInt(row.Priority.toString(), 10);
+                        var color;
+                        switch (priority) {
+                            case 1: color = '#F39092'; break;
+                            case 2: color = '#F5DC8F'; break;
+                            case 3: color = '#9EBACB'; break;
+                            case 4: color = '#DFE0DC'; break;
+                            default: color = 'white'; break;
+                        }
+                        var deltaTime = 0;
+                        if (typeof deltaTimeString === 'string') {
+                            deltaTime = +deltaTimeString.replace(',', '.');
+                        } else {
+                            deltaTime = deltaTimeString;
+                        }
+                        page = 0;
+                        technology = new TechRadar.Technology(
+                            id++,
+                            priority,
+                            row.Category,
+                            row.Thumbnail,
+                            row.TimeCategory,
+                            deltaTime,
+                            row.ShortTitle,
+                            row.Title,
+                            row.Subtitle,
+                            row.Text,
+                            color);
+                        this.technologies.push(technology);
+                    }
+
+                    if (row.ContentType === '') row.ContentType = 'markdown';
+                    if (row.Content !== '') {
+                        var c = new TechRadar.Content(page++, row.ContentType, row.Content, row.Subtitle);
+                        if (c.contentType.toLowerCase() === 'youtube') {
+                            c.videoUrl = c.content.indexOf('http') > 0
+                                ? c.content
+                                : 'http://www.youtube.com/embed/' + c.content + '?rel=0&autoplay=1';
+                            //console.log(c.videoUrl);
+                        };
+                        technology.content.push(c);
+                    }
+                });
+                callback();
+            });
+        }
+
         /**
          * Load a worksheet.
-         */ 
-         public loadSheet(url: string, callback: (sheet: ISpreadsheetRow[]) => void) {
+         */
+         private loadSheet(url: string, callback: (sheet: ISpreadsheetRow[]) => void) {
             console.log('Initializing tabletop');
             Tabletop.init({
                 key         : url,
